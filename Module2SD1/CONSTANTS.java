710a646e3845eddc0b76d0dc379baa9bdc6b0f6d
Ethan <ejnephew@yahoo.com>
2021-01-29 12:27:47 -0500

    Changed the file names in CONSTANTS

--- Module2SD1/CONSTANTS.java
+++ Module2SD1/CONSTANTS.java
@@ -1,8 +1,8 @@
 package Module2SD1;
 
 public interface CONSTANTS {
-    String downloadDirectory = "downloadedPage.txt";
-    String normalizedDirectory = "normalizedPage.txt";
-    String wordReportDirectory = "wordReport.txt";
+    String downloadDirectory = "EthanJNephewMod2SDLCDownloadedPage.txt";
+    String normalizedDirectory = "EthanJNephewMod2SDLCNormalizedPage.txt";
+    String wordReportDirectory = "EthanJNephewMod2SDLCWordReport.txt";
     String websiteURL = "https://www.gutenberg.org/files/1065/1065-h/1065-h.htm";
 }
